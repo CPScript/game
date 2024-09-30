af4591ef557bd6cfb1c3e484657647a24c76cdbc
Disease <83523587+CPScript@users.noreply.github.com>
2024-09-30 14:34:09 -0400

    Create fragment_shader.glsl

---
+++ shaders/fragment_shader.glsl
@@ -0,0 +1,29 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec3 FragPos;
+in vec3 Normal;
+in vec3 Color;
+
+uniform vec3 viewPos;
+
+void main() {
+    // Ambient
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * Color;
+
+    // Diffuse
+    vec3 lightDir = normalize(vec3(0.0, 1.0, 0.0) - FragPos);
+    float diff = max(dot(Normal, lightDir), 0.0);
+    vec3 diffuse = diff * Color;
+
+    // Specular
+    float specularStrength = 0.5;
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, Normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength * spec * vec3(1.0);
+
+    vec3 result = ambient + diffuse + specular;
+    FragColor = vec4(result, 1.0);
+}
