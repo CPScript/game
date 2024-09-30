36c21e8f7d67d1d526ad6081c2b05c2e6ad1d8b4
Disease <83523587+CPScript@users.noreply.github.com>
2024-09-30 14:34:40 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,18 @@
-hello world
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+float vertices[] = {
+    // positions          // colors           // normals
+    -0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.0f,  0.0f, 0.0f, -1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,  0.0f, 0.0f, -1.0f,
+     0.5f,  0.5f, -0.5f,  0.0f, 0.0f, 1.0f,  0.0f, 0.0f, -1.0f,
+     0.5f,  0.5f, -0.5f,  0.0f, 0.0f, 1.0f,  
